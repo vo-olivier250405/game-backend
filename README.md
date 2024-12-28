0f7546280ac061164836f61f451ca4a55bf3ada2
vo_o <vo_o@etna-alternant.net>
2024-12-28 16:26:38 +0100

    [ADD] => setup backend aplication

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # game-backend
+
+## Start project
+
+```sh
+cd backend
+pip3 install -r requirements.txt
+python3 manage.py runserver
+```
---
+++ backend/api/views/login_view.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import login, logout
+
+from knox.views import LoginView as KnoxLoginView
+from knox.views import LogoutView as KnoxLogoutView
+
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from api.serializers import UserSerializer
+from django.contrib.auth.models import User
+
+class LoginView(KnoxLoginView):
+    permission_classes = (AllowAny,)
+    
+    def get_user_serializer_class(self): return UserSerializer
+    
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data.get("user", None)
+        print(user)
+        login(request, user)
+        return super().post(request, format)
+
+
+class LogoutView(KnoxLogoutView):
+    permission_classes = (IsAuthenticated,)
+    
+    def post(self, request, format=None):
+        logout(request)
+        return super().post(request, format)
+        
